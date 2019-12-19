43891fd3109aff61c9a76df122c60505f765b1c9
Zhangqzh <54208163+Zhangqzh@users.noreply.github.com>
2019-12-19 23:13:58 +0800

    Add files via upload

---
+++ AR虚拟按键小游戏/Assets/Resources/Scripts/VirtualButtonEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Vuforia;
+[System.Obsolete]
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+
+    public VirtualButtonBehaviour vb;
+    public Animator animator;
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        animator.SetBool("walk", true);
+        Debug.Log("walk");
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        animator.SetBool("walk", false);
+        Debug.Log("stop");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
